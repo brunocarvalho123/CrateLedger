71772709e2d919b479bb9df6c02984846ad2cbd2
Bruno Carvalho <bruno.js.carvalho123@gmail.com>
2025-05-15 18:32:31 +0100

    New AssetImage view

---
+++ CrateLedger/Views/Components/Asset/AssetImage.swift
@@ -0,0 +1,18 @@
+//
+//  AssetImage.swift
+//  CrateLedger
+//
+//  Created by Bruno Carvalho on 15/05/2025.
+//
+
+import SwiftUI
+
+struct AssetImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AssetImage()
+}
