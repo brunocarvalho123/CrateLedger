b9fa6a2edd0aad8cc3c2e59a1aedfe02493c55ae
Bruno Carvalho <bruno.js.carvalho123@gmail.com>
2025-05-17 11:14:06 +0100

    Create a new component to manage portfolios

---
+++ CrateLedger/Views/Components/PortfolioPicker.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioPicker.swift
+//  CrateLedger
+//
+//  Created by Bruno Carvalho on 17/05/2025.
+//
+
+import SwiftUI
+
+struct PortfolioPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PortfolioPicker()
+}
